dd26efb607fbfd0976bf18d19eb6e69f75062eb4
CryptoCafeAdmin <jessecox711@gmail.com>
2023-02-06 00:43:09 +0500

    update

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -1,15 +1,15 @@
 const SEO = {
   defaultTitle: 'Crypto Cafe',
-  titleTemplate: '%s | Sushi',
+  titleTemplate: '%s | Crypto Cafe',
   description: 'Swap, yield, lend, borrow, leverage, limit, launch all on one community driven ecosystem',
   openGraph: {
     type: 'website',
     url: 'https://app.sushi.com/',
-    site_name: 'Sushi',
+    site_name: 'Crypto Cafe',
   },
   twitter: {
-    handle: '@SushiSwap',
-    site: '@SushiSwap',
+    handle: '@CryptoCafeEx',
+    site: '@CryptoCafeEx',
     cardType: 'summary_large_image',
   },
 }
